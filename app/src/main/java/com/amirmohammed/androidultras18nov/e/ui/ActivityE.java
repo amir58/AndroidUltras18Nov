8fef2f71092cee1b5f525e0d4ca61f62a64aca93
amir58 <devamir11@gmail.com>
2022-03-03 21:03:36 +0200

    Session 18

--- app/src/main/java/com/amirmohammed/androidultras18nov/e/ui/ActivityE.java
+++ app/src/main/java/com/amirmohammed/androidultras18nov/e/ui/ActivityE.java
@@ -2,15 +2,23 @@ package com.amirmohammed.androidultras18nov.e.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.amirmohammed.androidultras18nov.R;
+import com.amirmohammed.androidultras18nov.databinding.ActivityEBinding;
 import com.amirmohammed.androidultras18nov.e.models.profile.ProfileResponse;
 import com.amirmohammed.androidultras18nov.e.models.register.RegisterResponse;
 import com.amirmohammed.androidultras18nov.e.models.update_profile.UpdateProfileRequest;
 import com.amirmohammed.androidultras18nov.e.models.update_profile.UpdateProfileResponse;
 import com.amirmohammed.androidultras18nov.e.network.RetrofitClientE;
+import com.amirmohammed.androidultras18nov.e.ui.fragments.CallsFragment;
+import com.amirmohammed.androidultras18nov.e.ui.fragments.ChatsFragment;
+import com.amirmohammed.androidultras18nov.e.ui.fragments.StatusFragment;
+import com.google.android.material.button.MaterialButton;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,12 +26,88 @@ import retrofit2.Response;
 
 public class ActivityE extends AppCompatActivity {
     private static final String TAG = "ActivityE";
+    // ViewBinding => no findViewById more
+    // Fragments
+
+    ActivityEBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_e);
+        binding = ActivityEBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, new ChatsFragment())
+                .commit();
+
+        binding.btnChats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, new ChatsFragment())
+                        .commit();
+            }
+        });
+
+        binding.btnStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, new StatusFragment())
+                        .commit();
+            }
+        });
+
+        binding.btnCalls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, new CallsFragment())
+                        .commit();
+            }
+        });
+
+    }
+
+    void sharedPreferences(){
+
+        // Local database => Room ( SQLITE )
+
+        // Local database => SharedPreferences ( Key value )
+        // key language value ar
+        // key apiToken value kfbq7y3g97wwf
+        // key theme value light
+
+        SharedPreferences sharedPreferences = getSharedPreferences("userData", MODE_PRIVATE);
+
+        sharedPreferences.edit().putString("language", "ar").apply();
+
+        sharedPreferences.getString("language", "ar");
+
+        sharedPreferences.edit().putString("theme", "dark").apply();
+
+        sharedPreferences.getString("theme", "dark");
+
+        sharedPreferences.edit().putString("apiToken", "9r736t97623r796e326fg").apply();
+
+        String apiToken = sharedPreferences.getString("apiToken", "");
+
+        if (apiToken.isEmpty()){
+            // navigate to login activity
+        }
+        else {
+            // navigate to main activity
+        }
+
+        sharedPreferences.edit().putString("apiToken", "").apply();
+    }
+
+    void apis(){
         RetrofitClientE.getApi().getProfileData("L2J0cFgWjKjQ2MrSM9YNDbMbGICD0rZP9SN4muaSlB91QYvVtEhPlNLnrepwhcXs7SB0PF")
                 .enqueue(new Callback<ProfileResponse>() {
                     @Override
---
+++ app/src/main/java/com/amirmohammed/androidultras18nov/e/ui/fragments/ChatsFragment.java
@@ -0,0 +1,35 @@
+package com.amirmohammed.androidultras18nov.e.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.amirmohammed.androidultras18nov.R;
+import com.amirmohammed.androidultras18nov.e.ui.ActivityE;
+
+
+public class ChatsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chats, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Toast.makeText(requireContext(), "Chats", Toast.LENGTH_SHORT).show();
+
+    }
+
+}
